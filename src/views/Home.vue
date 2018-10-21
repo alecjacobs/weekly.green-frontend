bde3a092d44d2e3b29e38e4cc8e38e75650e5bfd
Andrew Cantino <cantino@users.noreply.github.com>
2018-10-21 12:04:49 -0700

    future

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <CashFlowGraph :height="150" :chartData="cashFlowHistory" :options="options" />
     </v-flex>
     <v-flex xs6>
-      <CashFlowGraph :height="150" :chartData="cashFlowHistory" :options="options" />
+      <CashFlowGraph :height="150" :chartData="future" :options="options" />
     </v-flex>
   </v-layout></v-container>
 </template>
@@ -21,6 +21,7 @@
     data () {
       return {
         cashFlowHistory: {},
+        future: {},
         options: {}
       }
     },
@@ -28,14 +29,50 @@
       axios.get('https://weekly-green.herokuapp.com/yodlee_mock.json').then(({ data }) => {
         let day = -1;
         let bals = [];
+        let future = [];
         data.transactions.forEach((transaction) => {
           const { bal, date } = transaction;
           let newDay = moment(date).day();
           if (newDay !== day) {
             day = newDay;
-            bals.push({ y: bal + 1000.0, t: date });
+            bals.push({ y: bal + 1000.0, t: moment(date) });
+            future.push({ y: bal + 1000.0, t: moment(date) });
           }
         });
+
+        let i = 0;
+        let b = bals[bals.length - 1].y;
+        this.future = {
+          datasets: [
+            {
+              label: 'Transaction History',
+              backgroundColor: '#aaa',
+              data: future.map(({ y, t }) => {
+                i = i + 1;
+                b = b + (Math.random() - 0.5) * 50.0;
+                return { y: b, t: t };
+              })
+            },
+            {
+              label: 'Transaction History',
+              backgroundColor: '#bbb',
+              data: future.map(({ y, t }) => {
+                i = i + 1;
+                b = b + (Math.random() - 0.5) * 40.0;
+                return { y: b, t: t };
+              })
+            },
+            {
+              label: 'Transaction History',
+              backgroundColor: '#ccc',
+              data: future.map(({ y, t }) => {
+                i = i + 1;
+                b = b + (Math.random() - 0.5) * 30.0;
+                return { y: b, t: t };
+              })
+            }
+          ]
+        };
 
         this.cashFlowHistory = {
           datasets: [
@@ -68,7 +105,7 @@
             yAxes: [{
               display: true,
               scaleLabel: {
-                display: true,
+                display: false,
                 labelString: 'Your Balance'
               }
             }]
