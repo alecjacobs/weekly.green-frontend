3590e89603adebcca3043e85225c76283bab0ce8
Alec Jacobs <jacobs.alec@gmail.com>
2018-10-21 11:28:34 -0700

    make cash flow graph component reusable

--- src/components/CashFlowGraph.vue
+++ src/components/CashFlowGraph.vue
@@ -1,54 +1,13 @@
 <script>
 import VueChart from 'vue-chartjs'
-import moment from 'moment'
-import axios from 'axios'
 
 export default {
   extends: VueChart.Line,
-  mixins: [VueChart.mixins.reactiveData],
-  data () {
-    return {
-      chartData: {
-        labels: [],
-        datasets: [
-          {
-            label: 'Transaction History',
-            backgroundColor: '#7af49b',
-            data: []
-          }
-        ]
-      }
-    }
-  },
+  props: ['chartData'],
+  mixins: [VueChart.mixins.reactiveProp],
   mounted () {
-    // window.Chart.scaleService.updateScaleDefaults('linear', { ticks: { min: -100 }})
     window.Chart.defaults.global.defaultFontColor = 'rgba(210, 210, 210, 0.9)'
     window.Chart.defaults.global.defaultColor = 'rgba(210, 210, 210, 0.9)'
-
-    axios.get('https://weekly-green.herokuapp.com/yodlee_mock.json').then(({ data }) => {
-      let day = -1;
-      let bals = [];
-      let days = [];
-      data.transactions.forEach((transaction) => {
-        const { bal, date } = transaction;
-        let newDay = moment(date).day();
-        if (newDay !== day) {
-          day = newDay;
-          days.push(moment(transaction.date).toDate());
-          bals.push({ y: transaction.bal + 1000.0, t: date });
-        }
-      });
-
-      this.chartData = {
-        datasets: [
-          {
-            label: 'Transaction History',
-            backgroundColor: '#7af49b',
-            data: bals
-          }
-        ]
-      };
-    });
 
     this.renderChart(this.chartData, {
       legend: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <CashFlowGraph :height="150" />
+  <CashFlowGraph :height="150" :chartData="cashFlowHistory" />
 </template>
 
 <script>
+  import axios from 'axios'
+  import moment from 'moment'
   import CashFlowGraph from '../components/CashFlowGraph'
 
   export default {
     components: {
       CashFlowGraph
+    },
+    data () {
+      return {
+        cashFlowHistory: {}
+      }
+    },
+    mounted () {
+      axios.get('https://weekly-green.herokuapp.com/yodlee_mock.json').then(({ data }) => {
+        let day = -1;
+        let bals = [];
+        let days = [];
+        data.transactions.forEach((transaction) => {
+          const { bal, date } = transaction;
+          let newDay = moment(date).day();
+          if (newDay !== day) {
+            day = newDay;
+            days.push(moment(transaction.date).toDate());
+            bals.push({ y: transaction.bal + 1000.0, t: date });
+          }
+        });
+
+        this.cashFlowHistory = {
+          datasets: [
+            {
+              label: 'Transaction History',
+              backgroundColor: '#7af49b',
+              data: bals
+            }
+          ]
+        };
+      });
     }
   }
 </script>
